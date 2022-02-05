78cbd3e98b2c4c6c8a0c2db9eeda7621ff9c23fe
David Tavares <davidtavares27@yahoo.com>
2022-02-05 20:00:59 -0300

    Iniciando o desafio

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Padaria do Seu Zé");
+
+Console.WriteLine("Digite o nome do cliente");
+var nomeDoCliente = Console.ReadLine();
+
+Console.WriteLine("Digite o nome do produto");
+var nomeDoProduto = Console.ReadLine();
+
+Console.WriteLine("Digite a quatidade do produto");
+var quatidadeDoproduto = Console.ReadLine();
+
+
+Console.WriteLine($"Cliente: {nomeDoCliente}");
+Console.WriteLine($"Pedido: {quatidadeDoproduto} {nomeDoProduto}");
+
+
+/*  Sistema de padaria:
+
+Digitar o nome do cliente
+Digitar o nome do produto
+Digitar a quantidade do produto
+
+
+
+Resultado esperado:
+
+Cliente: Gustavo
+Pedido: 10 Pães francês  */
